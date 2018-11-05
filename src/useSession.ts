60b0891f059450ddd4dbb855624c1afe6f9758cc
GabrielBB03 <gabrielbb0306@gmail.com>
2018-11-05 14:23:46 -0400

    Adding javascript documentation to useSession function

--- src/useSession.ts
+++ src/useSession.ts
@@ -1,6 +1,12 @@
 import { useState, useEffect } from './react';
 import parseJWT from './util/jwt-parse';
 
+/**
+* React hook to handle session from your browser storage
+* @param sessionKey This required parameter is used as the browser storage key. This should be a unique string per app. For example your package.json's name value.
+* @param keepOnBrowserClosed This optional parameter will determine if useSession will work with LocalStorage or SessionStorage. Default value is true, so default storage is LocalStorage
+* @returns If there is a session in your browser storage then it will be returned. If there is no session, it will return null.
+*/
 function useSession(sessionKey: string, keepOnBrowserClosed: boolean = true)
   : { session: object | string | null, save: (sessionValue: object | string) => void, saveJWT: (jwt: string) => void, clear: () => void } {
 
